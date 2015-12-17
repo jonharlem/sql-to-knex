95d6df51fbb6cdba2cc032ef154bd4f2c15bff1b
Jon Harlem <jtharlem@gmail.com>
2015-12-17 11:42:00 -0800

    min from, whereNotNull, whereIn orWhereIn complete

--- sqlKnex.js
+++ sqlKnex.js
@@ -9,9 +9,13 @@ knex('students').where({id:5}).limit(1);
 // SELECT COUNT(*) students;
 select count(*) from "students";
 // SELECT MIN('year') FROM students;
+select min("year") from "students";
 // SELECT * FROM students WHERE name IS NOT NULL;
+knex('students').whereNotNull('name');
 // SELECT * FROM todos WHERE id IN ('1', '2', '3') OR user_id IN ('4', '5', '6');
+knex.select('*').from('todos').whereIn('id', ['1','2','3']).orWhereIn('user_id', ['4','5','6']);
 // SELECT * FROM students LIMIT 10 OFFSET 30;
+
 // INSERT INTO students (name,fav_color) VALUES ('tyler','purple');
 // INSERT INTO students (name,fav_color) VALUES ('liz','blue') RETURNING *;
 // UPDATE students SET name ='cho' WHERE id=5;
