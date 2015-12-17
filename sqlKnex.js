ae3cc032e31daaf779e41f5b9489f781c14e091c
Jon Harlem <jtharlem@gmail.com>
2015-12-17 11:52:59 -0800

    added numbers to the problems and completed solutions for numbers 8,9,10

--- sqlKnex.js
+++ sqlKnex.js
@@ -1,28 +1,31 @@
 // Turn the following SQL queries into Knex queries (you can write them next to each SQL query or below):
 
-// SELECT * FROM students;
+//1. SELECT * FROM students;
 knex('students');
-// SELECT * FROM students WHERE id=1;
+//2. SELECT * FROM students WHERE id=1;
 knex('students').where({id:1});
-// SELECT * FROM students WHERE id=5; LIMIT 1
+//3. SELECT * FROM students WHERE id=5; LIMIT 1
 knex('students').where({id:5}).limit(1);
-// SELECT COUNT(*) students;
+//4. SELECT COUNT(*) students;
 select count(*) from "students";
-// SELECT MIN('year') FROM students;
+//5. SELECT MIN('year') FROM students;
 select min("year") from "students";
-// SELECT * FROM students WHERE name IS NOT NULL;
+//6. SELECT * FROM students WHERE name IS NOT NULL;
 knex('students').whereNotNull('name');
-// SELECT * FROM todos WHERE id IN ('1', '2', '3') OR user_id IN ('4', '5', '6');
+//7. SELECT * FROM todos WHERE id IN ('1', '2', '3') OR user_id IN ('4', '5', '6');
 knex.select('*').from('todos').whereIn('id', ['1','2','3']).orWhereIn('user_id', ['4','5','6']);
-// SELECT * FROM students LIMIT 10 OFFSET 30;
+//8. SELECT * FROM students LIMIT 10 OFFSET 30;
+knex.select('*').from('todos').limit(10).offset(30);
+//9. INSERT INTO students (name,fav_color) VALUES ('tyler','purple');
+knex('students').insert({name: 'tyler',fav_color: 'purple'});
+//10. INSERT INTO students (name,fav_color) VALUES ('liz','blue') RETURNING *;
+knex('students').returning('*').insert({name: 'liz',fav_color: 'blue'});
+//11. UPDATE students SET name ='cho' WHERE id=5;
+//12. DELETE * FROM students;
+//13. UPDATE "students" SET "score" = "score" + 10 WHERE id=1;
+//14. SELECT * FROM "students" LEFT OUTER JOIN "todos" ON "students"."id" = "todos"."student_id".
+//15. SELECT * FROM "students" RIGHT OUTER JOIN "todos" ON "students"."id" = "todos"."student_id";
 
-// INSERT INTO students (name,fav_color) VALUES ('tyler','purple');
-// INSERT INTO students (name,fav_color) VALUES ('liz','blue') RETURNING *;
-// UPDATE students SET name ='cho' WHERE id=5;
-// DELETE * FROM students;
-// UPDATE "students" SET "score" = "score" + 10 WHERE id=1;
-// SELECT * FROM "students" LEFT OUTER JOIN "todos" ON "students"."id" = "todos"."student_id".
-// SELECT * FROM "students" RIGHT OUTER JOIN "todos" ON "students"."id" = "todos"."student_id";
 // Answer the following questions:
 
 // See the documentation for pluck - when would a method like this be useful?
