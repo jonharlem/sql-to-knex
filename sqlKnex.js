097c29317679746449ac7a02f8f1e09b4753213e
Jon Harlem <jtharlem@gmail.com>
2015-12-17 11:32:16 -0800

    initial commit

---
+++ sqlKnex.js
@@ -0,0 +1,27 @@
+// Turn the following SQL queries into Knex queries (you can write them next to each SQL query or below):
+
+// SELECT * FROM students;
+
+// SELECT * FROM students WHERE id=1;
+
+// SELECT * FROM students WHERE id=5; LIMIT 1
+
+// SELECT COUNT(*) students;
+
+// SELECT MIN('year') FROM students;
+// SELECT * FROM students WHERE name IS NOT NULL;
+// SELECT * FROM todos WHERE id IN ('1', '2', '3') OR user_id IN ('4', '5', '6');
+// SELECT * FROM students LIMIT 10 OFFSET 30;
+// INSERT INTO students (name,fav_color) VALUES ('tyler','purple');
+// INSERT INTO students (name,fav_color) VALUES ('liz','blue') RETURNING *;
+// UPDATE students SET name ='cho' WHERE id=5;
+// DELETE * FROM students;
+// UPDATE "students" SET "score" = "score" + 10 WHERE id=1;
+// SELECT * FROM "students" LEFT OUTER JOIN "todos" ON "students"."id" = "todos"."student_id".
+// SELECT * FROM "students" RIGHT OUTER JOIN "todos" ON "students"."id" = "todos"."student_id";
+// Answer the following questions:
+
+// See the documentation for pluck - when would a method like this be useful?
+// How do you specify that a column must be unique using Knex?
+// How do you specify that a column can not be NULL using Knex?
+// Can you also write raw SQL within knex queries? If so, how do you do that?
